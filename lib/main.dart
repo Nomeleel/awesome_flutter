5b671bcebc183c0c6f130e0855db41dbf0d5b498
Nomeleel <choonlay@outlook.com>
2019-11-20 23:22:20 +0800

    add navigator.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:awesome_flutter/waterWaveView.dart';
+import 'package:awesome_flutter/yinYangSwitchView.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  static Map<String, WidgetBuilder> routes = Map<String, WidgetBuilder>();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,6 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: "Home"),
+      routes: routes,
     );
   }
 }
@@ -25,50 +29,57 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  List pages;
+  _MyHomePageState();
+
+  Map<String, WidgetBuilder> routes = MyApp.routes;
 
   @override
   void initState() {
-    pages = List();
-    pages.add("1");
-    pages.add("2");
-    pages.add("3");
-    pages.add("4");
+    // TODO 规范化 根据目录下的文件自动构建路由
+    routes["/pages/waterWaveView"] = (BuildContext context) => WaterWaveView();
+    routes["/pages/yinYangSwitchView"] = (BuildContext context) => YinYangSwitchView();
+
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: pages.length,
+      itemCount: routes.length,
       itemBuilder: (BuildContext context, int index) {
         return Padding(
           padding: EdgeInsets.all(15),
-          child: ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(20.0)),
-            child: Container(
-              height: MediaQuery.of(context).size.width * 0.618 + 50,
-              color: Colors.deepOrange,
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    height: (MediaQuery.of(context).size.width - 30) * 0.618 - 30,
-                    color: Colors.green,
-                  ),
-                  Container(
-                    height: 33,
-                    color: Colors.purple,
-                  ),
-                  Container(
-                    height: 33,
-                    color: Colors.blue,
-                    child: Text(
-                      pages[index] * 30,
-                      style: TextStyle(
-                        fontSize: 30,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(routes.keys.elementAt(index));
+            },
+            child:  ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+              child: Container(
+                height: MediaQuery.of(context).size.width * 0.618 + 50,
+                color: Colors.deepOrange,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: (MediaQuery.of(context).size.width - 30) * 0.618 - 30,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      height: 33,
+                      color: Colors.purple,
+                    ),
+                    Container(
+                      height: 33,
+                      color: Colors.blue,
+                      child: Text(
+                        routes.keys.elementAt(index),
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
