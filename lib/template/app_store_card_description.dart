6b87b98a46f50b604204d9fdea47ff0d477db2ba
Nomeleel <choonlay@outlook.com>
2020-04-22 23:16:16 +0800

    Imp classic mode of app store card description.

---
+++ lib/template/app_store_card_description.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AppStoreCardDescription extends StatelessWidget {
+
+  const AppStoreCardDescription({
+    Key key,
+    this.mode,
+    this.data,
+  }) : super(key: key);
+
+  final AppStoreCardDescriptionMode mode;
+  final AppStoreCardDescriptionData data;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget description;
+
+    switch (mode) {
+      case AppStoreCardDescriptionMode.classic:
+      default:
+        description = getClassicDescription();
+    }
+
+    return description;
+  }
+
+  Widget getClassicDescription() {
+    var subText = (text) => Text(
+      text,
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(
+        decoration: TextDecoration.none,
+        color: Colors.grey[500],
+        fontSize: 13,
+      ),
+    );
+
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          subText(data.label),
+          Text(
+            data.title,
+            style: TextStyle(
+              decoration: TextDecoration.none,
+              color: Colors.black,
+              fontSize: 27,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          Spacer(),
+          subText(data.info),
+        ],
+      ),
+    );
+  }
+}
+
+enum AppStoreCardDescriptionMode {
+  /* Classic Mode
+    ————————————————————————————————
+    | label                        |
+    | TitleTitleTitleTitleTitle    |
+    | Title                        |
+    |                              |
+    |                              |
+    |                              |
+    |                              |
+    |                              |
+    | info                         |
+    ————————————————————————————————
+  */
+  classic,
+}
+
+class AppStoreCardDescriptionData {
+
+  const AppStoreCardDescriptionData({
+    this.label,
+    this.title,
+    this.info,
+  });
+
+  final String title;
+  final String label;
+  final String info;
+}
