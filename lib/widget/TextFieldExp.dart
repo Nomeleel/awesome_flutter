7d9d1d23b4756bb31d06f5abdf21dff44f931f28
Nomeleel <choonlay@outlook.com>
2020-10-05 22:23:59 +0800

    TextFieldExp Widget.

---
+++ lib/widget/TextFieldExp.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class TextFieldExp extends StatefulWidget {
+  TextFieldExp({Key key}) : super(key: key);
+
+  @override
+  _TextFieldExpState createState() => _TextFieldExpState();
+}
+
+class _TextFieldExpState extends State<TextFieldExp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.purple,
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          height: 50.0,
+          child: TextField(
+            autofocus: true,
+          ),
+        ),
+      ),
+    );
+  }
+}
