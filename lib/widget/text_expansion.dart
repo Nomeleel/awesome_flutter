b51fe40ed1772216f3aec14a3caa0185d0087824
Nomeleel <choonlay@outlook.com>
2020-10-20 23:19:53 +0800

    TextExpansion refine.

--- lib/widget/text_expansion.dart
+++ lib/widget/text_expansion.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class TextExpansion extends StatefulWidget {
-  TextExpansion({Key key, this.text}) : super(key: key);
+  const TextExpansion(this.text, this.maxLines, {Key key, this.maxWidth, this.style, this.expansionWidget})
+      : super(key: key);
 
   final String text;
+  final int maxLines;
+  final TextStyle style;
+  final double maxWidth;
+  final Widget expansionWidget;
 
   @override
   TextExpansionState createState() => TextExpansionState();
@@ -12,66 +17,62 @@ class TextExpansion extends StatefulWidget {
 
 class TextExpansionState extends State<TextExpansion> {
   bool _expand = false;
+  bool _didExceedMaxLines;
 
   @override
-  Widget build(BuildContext context) {
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    _didExceedMaxLines = didExceedMaxLines();
+  }
+
+  bool didExceedMaxLines() {
     final TextPainter textPainter = TextPainter(
-      maxLines: 2,
+      maxLines: widget.maxLines,
       text: TextSpan(
         text: widget.text,
-        style: TextStyle(
-          fontSize: 14.0,
-          color: Colors.black,
-        ),
+        style: widget.style,
       ),
       textDirection: TextDirection.ltr,
     );
-    textPainter.layout(maxWidth: MediaQuery.of(context).size.width);
-    if (!textPainter.didExceedMaxLines) {
-      return Text(
-        widget.text,
-        style: TextStyle(
-          fontSize: 14.0,
-          color: Colors.black,
-        ),
-      );
-    } else {
+    // 可以获取父容器的宽度，最大不超过屏幕宽度
+    textPainter.layout(maxWidth: widget.maxWidth ?? MediaQuery.of(context).size.width);
+
+    return textPainter.didExceedMaxLines;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_didExceedMaxLines) {
       if (_expand) {
-        return switchWidget(
-          Text(
-            widget.text,
-            style: TextStyle(
-              fontSize: 14.0,
-              color: Colors.black,
-            ),
-          ),
-        );
+        return switchWidget(text());
       } else {
-        return Stack(
-          children: [
-            Text(
-              widget.text,
-              maxLines: 2,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.black,
-              ),
-            ),
-            Positioned(
-              bottom: 0.0,
-              right: 0.0,
-              child: switchWidget(
-                Container(
-                  height: 30.0,
-                  width: 30.0,
-                  color: Colors.purple,
-                ),
-              ),
-            )
-          ],
-        );
+        if (widget.expansionWidget == null) {
+          return switchWidget(text(widget.maxLines));
+        } else {
+          return Stack(
+            children: [
+              text(widget.maxLines),
+              Positioned(
+                bottom: 0.0,
+                right: 0.0,
+                child: switchWidget(widget.expansionWidget),
+              )
+            ],
+          );
+        }
       }
+    } else {
+      return text();
     }
+  }
+
+  Text text([int maxLines]) {
+    return Text(
+      widget.text,
+      maxLines: maxLines,
+      style: widget.style,
+    );
   }
 
   Widget switchWidget(Widget child) {
