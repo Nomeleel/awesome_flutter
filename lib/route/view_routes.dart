4944970bf4161a22da50d13bea04b3084d96c326
Nomeleel <choonlay@outlook.com>
2021-03-02 22:07:05 +0800

    路由相关修改

---
+++ lib/route/view_routes.dart
@@ -0,0 +1,17 @@
+/// *****************************
+
+/// Generated file. Do not edit.
+
+/// *****************************
+
+import 'package:flutter/widgets.dart';
+
+import 'view_export_list.dart';
+
+Map<String, WidgetBuilder> viewRoutes = <String, WidgetBuilder>{
+  'grid_paper_exp_view': (BuildContext context) =>  GridPaperExpView(),
+  'yin_yang_switch_view': (BuildContext context) =>  YinYangSwitchView(),
+  'water_wave_view': (BuildContext context) =>  WaterWaveView(),
+  'sliver_app_bar_exp_view': (BuildContext context) =>  SliverAppBarExpView(),
+  'creative_stitching_view': (BuildContext context) =>  CreativeStitchingView(),
+};
