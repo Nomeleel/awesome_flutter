3ed6fecb5329d57947f3c93f153b6b267455f12e
Nomeleel <choonlay@outlook.com>
2021-06-01 21:50:01 +0800

    Simulation 不敢乱动， 莫名其妙的数字。

---
+++ lib/view/physics_test_view.dart
@@ -0,0 +1,179 @@
+import 'dart:math' as math;
+
+import '../widget/scaffold_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class PhysicsTestView extends StatefulWidget {
+  const PhysicsTestView({Key key}) : super(key: key);
+
+  @override
+  _PhysicsTestViewState createState() => _PhysicsTestViewState();
+}
+
+class _PhysicsTestViewState extends State<PhysicsTestView> {
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldView(
+      title: 'Physics Test View',
+      body: ListView.builder(
+        //physics: ClampingScrollPhysics(),
+        physics: FastScrollPhysics(),
+        itemExtent: 77.0,
+        itemCount: 777,
+        itemBuilder: (BuildContext context, int index) => DecoratedBox(
+          decoration: BoxDecoration(color: Colors.primaries[index % Colors.primaries.length]),
+          child: Center(
+            child: Text('$index'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FastScrollPhysics extends ScrollPhysics {
+  const FastScrollPhysics({ ScrollPhysics parent }) : super(parent: parent);
+
+  @override
+  FastScrollPhysics applyTo(ScrollPhysics ancestor) {
+    return FastScrollPhysics(parent: buildParent(ancestor));
+  }
+
+  @override
+  double applyBoundaryConditions(ScrollMetrics position, double value) {
+    if (value < position.pixels && position.pixels <= position.minScrollExtent) // underscroll
+      return value - position.pixels;
+    if (position.maxScrollExtent <= position.pixels && position.pixels < value) // overscroll
+      return value - position.pixels;
+    if (value < position.minScrollExtent && position.minScrollExtent < position.pixels) // hit top edge
+      return value - position.minScrollExtent;
+    if (position.pixels < position.maxScrollExtent && position.maxScrollExtent < value) // hit bottom edge
+      return value - position.maxScrollExtent;
+    return 0.0;
+  }
+
+  @override
+  Simulation createBallisticSimulation(ScrollMetrics position, double velocity) {
+    final Tolerance tolerance = this.tolerance;
+    if (position.outOfRange) {
+      double end;
+      if (position.pixels > position.maxScrollExtent)
+        end = position.maxScrollExtent;
+      if (position.pixels < position.minScrollExtent)
+        end = position.minScrollExtent;
+      assert(end != null);
+      return ScrollSpringSimulation(
+        spring,
+        position.pixels,
+        end,
+        math.min(0.0, velocity),
+        tolerance: tolerance,
+      );
+    }
+    if (velocity.abs() < tolerance.velocity)
+      return null;
+    if (velocity > 0.0 && position.pixels >= position.maxScrollExtent)
+      return null;
+    if (velocity < 0.0 && position.pixels <= position.minScrollExtent)
+      return null;
+    return FastScrollSimulation(
+      position: position.pixels,
+      velocity: velocity,
+      tolerance: tolerance,
+      //friction: 0.007
+    );
+  }
+}
+
+// ClampingScrollSimulation
+// simplifications have been made.
+class FastScrollSimulation extends Simulation {
+  /// Creates a scroll physics simulation that matches Android scrolling.
+  FastScrollSimulation({
+    @required this.position,
+    @required this.velocity,
+    this.friction = 0.015,
+    Tolerance tolerance = Tolerance.defaultTolerance,
+  }) : assert(_flingVelocityPenetration(0.0) == _initialVelocityPenetration),
+       super(tolerance: tolerance) {
+    _duration = _flingDuration(velocity);
+    _distance = (velocity * _duration / _initialVelocityPenetration).abs();
+  }
+
+  /// The position of the particle at the beginning of the simulation.
+  final double position;
+
+  /// The velocity at which the particle is traveling at the beginning of the
+  /// simulation.
+  final double velocity;
+
+  /// The amount of friction the particle experiences as it travels.
+  ///
+  /// The more friction the particle experiences, the sooner it stops.
+  final double friction;
+
+  double _duration;
+  double _distance;
+
+  // See DECELERATION_RATE.
+  static final double _kDecelerationRate = math.log(0.78) / math.log(0.9);
+
+  // See computeDeceleration().
+  static double _decelerationForFriction(double friction) {
+    return friction * 61774.04968;
+  }
+
+  // See getSplineFlingDuration(). Returns a value in seconds.
+  double _flingDuration(double velocity) {
+    // See mPhysicalCoeff
+    final double scaledFriction = friction * _decelerationForFriction(0.84);
+
+    // See getSplineDeceleration().
+    final double deceleration = math.log(0.35 * velocity.abs() / scaledFriction);
+
+    return math.exp(deceleration / (_kDecelerationRate - 1.0));
+  }
+
+  // Based on a cubic curve fit to the Scroller.computeScrollOffset() values
+  // produced for an initial velocity of 4000. The value of Scroller.getDuration()
+  // and Scroller.getFinalY() were 686ms and 961 pixels respectively.
+  //
+  // Algebra courtesy of Wolfram Alpha.
+  //
+  // f(x) = scrollOffset, x is time in milliseconds
+  // f(x) = 3.60882×10^-6 x^3 - 0.00668009 x^2 + 4.29427 x - 3.15307
+  // f(x) = 3.60882×10^-6 x^3 - 0.00668009 x^2 + 4.29427 x, so f(0) is 0
+  // f(686ms) = 961 pixels
+  // Scale to f(0 <= t <= 1.0), x = t * 686
+  // f(t) = 1165.03 t^3 - 3143.62 t^2 + 2945.87 t
+  // Scale f(t) so that 0.0 <= f(t) <= 1.0
+  // f(t) = (1165.03 t^3 - 3143.62 t^2 + 2945.87 t) / 961.0
+  //      = 1.2 t^3 - 3.27 t^2 + 3.065 t
+  static const double _initialVelocityPenetration = 3.065;
+  static double _flingDistancePenetration(double t) {
+    return (1.2 * t * t * t) - (3.27 * t * t) + (_initialVelocityPenetration * t);
+  }
+
+  // The derivative of the _flingDistancePenetration() function.
+  static double _flingVelocityPenetration(double t) {
+    return (3.6 * t * t) - (6.54 * t) + _initialVelocityPenetration;
+  }
+
+  @override
+  double x(double time) {
+    final double t = (time / _duration).clamp(0.0, 1.0);
+    return position + _distance * _flingDistancePenetration(t) * velocity.sign;
+  }
+
+  @override
+  double dx(double time) {
+    final double t = (time / _duration).clamp(0.0, 1.0);
+    return _distance * _flingVelocityPenetration(t) * velocity.sign / _duration;
+  }
+
+  @override
+  bool isDone(double time) {
+    return time >= _duration;
+  }
+}
