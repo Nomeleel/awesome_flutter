ee218a2e9d3cf37af8ee31ca8b65eb0f34b0c696
Nomeleel <choonlay@outlook.com>
2021-07-07 23:57:21 +0800

    GalleryView scale test.

--- lib/widget/gallery_view.dart
+++ lib/widget/gallery_view.dart
@@ -20,29 +20,39 @@ class GalleryView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ValueNotifier<int> count = ValueNotifier<int>(minPreRow);
+    final ValueNotifier<double> scale = ValueNotifier<double>(1.0);
     int startCount = minPreRow;
+    double startScale = 1.0;
     return Scaffold(
       body: GestureDetector(
-        onScaleStart: (e) => startCount = count.value,
+        onScaleStart: (e) {
+          startCount = count.value;
+          startScale = scale.value;
+        },
         onScaleUpdate: (ScaleUpdateDetails details) {
           count.value = (startCount * (1.0 / details.scale) ~/ 1).clamp(minPreRow, maxPreRow);
+          scale.value = startScale * details.scale;
         },
         child: ValueListenableBuilder(
-          valueListenable: count,
-          builder: (_, int value, __) {
-            return GridView.builder(
-              // TODO: 转变太快, 并且一行不能显示半个然后自动校准
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: value),
-              itemCount: itemCount,
-              itemBuilder: (BuildContext context, int index) {
-                return AnimatedSwitcher(
-                  duration: duration,
-                  child: Container(
-                    key: ValueKey('${index ~/ value}-${index % value}'),
-                    child: itemBuilder(context, index),
-                  ),
-                );
-              },
+          valueListenable: scale,
+          builder: (_, double value, __) {
+            return Transform.scale(
+              scale: value,
+              alignment: Alignment.topLeft,
+              child: GridView.builder(
+                // TODO: 转变太快, 并且一行不能显示半个然后自动校准
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: count.value),
+                itemCount: itemCount,
+                itemBuilder: (BuildContext context, int index) {
+                  return AnimatedSwitcher(
+                    duration: duration,
+                    child: Container(
+                      key: ValueKey('${index ~/ value}-${index % value}'),
+                      child: itemBuilder(context, index),
+                    ),
+                  );
+                },
+              ),
             );
           },
         ),
