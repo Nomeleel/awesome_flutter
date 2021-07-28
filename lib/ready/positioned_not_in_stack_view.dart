4e416ae2303b992595fc0388c44972e9f19f66a4
Nomeleel <choonlay@outlook.com>
2021-07-28 23:41:42 +0800

    Positioned Not In Stack View temp.

---
+++ lib/ready/positioned_not_in_stack_view.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import '../widget/scaffold_view.dart';
+
+class PositionedNotInStackView extends StatelessWidget {
+  const PositionedNotInStackView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldView(
+      title: 'Positioned Not In Stack View',
+      body: Column(
+        children: [
+          PositionedWrap(
+            child: _container(),
+          ),
+          Stack(
+            children: [
+              PositionedWrap(
+                child: _container(),
+              ),
+              FlutterLogo(size: 200.0),
+              Stack(
+                children: [
+                  PositionedWrap(
+                    child: _container(),
+                  ),
+                  FlutterLogo(size: 220.0),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _container() {
+    return Container(
+      height: 200.0,
+      width: 200.0,
+      color: Colors.purple,
+    );
+  }
+}
+
+class PositionedWrap extends StatelessWidget {
+  const PositionedWrap({Key key, this.child}) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final isInStack = context.findAncestorWidgetOfExactType<Stack>().children.any((e) => e == this);
+    if (isInStack) {
+      return Positioned(
+        top: 20.0,
+        left: 20.0,
+        child: child,
+      );
+    }
+    return child;
+  }
+}
