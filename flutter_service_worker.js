3cf8c2aa67285a07b2c780e203c181a56a95bc89
Nomeleel <choonlay@outlook.com>
2021-08-29 00:00:57 +0800

    Publish Flutter web page to github page.
    Branch: master
    Commit: dc19dc2a04ebbf787d3dc9586c0a5910e3f536be (dirty)
    
    package:peanut 4.0.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,18 +4,18 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "9d549dfb7ea5500e0ed5b99f94744b03",
-"index.html": "031096ce7d424b00c03abb49c77ebca3",
-"/": "031096ce7d424b00c03abb49c77ebca3",
-"main.dart.js": "5be0038347ee35c8d7a470ebda7aa02d",
+"index.html": "fc186d8c305110c687eceb522410f577",
+"/": "fc186d8c305110c687eceb522410f577",
+"main.dart.js": "e65245e699c0a1bcc8d3bc65ccc596d2",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "2d355e93f130311b0a55c6feedd7ae35",
 "assets/AssetManifest.json": "6fcf0d233ce7ab568f40286284f9577d",
-"assets/NOTICES": "c2ae01ac0ca58f81fa9c4409a3f40fc4",
+"assets/NOTICES": "bb7952e9f6924e92c6e1695b3f5a0ea5",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/assets/images/iPhoneCase12.png": "31812536151688de10c33acca3865fbc",
 "assets/assets/images/BigSurBG.jpg": "f6671a4ae2a8b1311b8203406e2e51e8",
 "assets/assets/images/YanLu.gif": "f17568ecf35581c1fc1e22f9f68a18b9",
@@ -41,7 +41,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
