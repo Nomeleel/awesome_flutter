407b58e162296e099e59baab0269ac1a61ae3199
Nomeleel <choonlay@outlook.com>
2021-08-29 00:04:45 +0800

    Update use local peanut.

--- tool/publish_to_github_page.sh
+++ tool/publish_to_github_page.sh
@@ -1,5 +1,5 @@
 # 后期可采用GitHub Action
 
 # flutter pub global activate peanut
-flutter pub global run peanut:peanut -m 'Publish Flutter web page to github page.'
+flutter pub run peanut:peanut -m 'Publish Flutter web page to github page.'
 git push origin --set-upstream gh-pages
