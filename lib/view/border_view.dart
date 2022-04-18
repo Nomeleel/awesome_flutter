3864784b455f93e31dab9e18b25346a4904defb8
Nomeleel <choonlay@outlook.com>
2022-04-18 21:36:30 +0800

    Border paint by path metric.

--- lib/view/border_view.dart
+++ lib/view/border_view.dart
@@ -6,30 +6,47 @@ class BorderView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 370,
-        height: 100,
-        decoration: ShapeDecoration(
-          color: Colors.black,
-          shape: SweepStadiumBorder(side: BorderSide(width: 5)),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Container(
+          width: 350,
+          height: 100,
+          decoration: ShapeDecoration(
+            color: Colors.black,
+            shape: SweepStadiumBorder(side: BorderSide(width: 30)),
+          ),
         ),
-      ),
+        Container(
+          width: 350,
+          height: 100,
+          decoration: ShapeDecoration(
+            color: Colors.black,
+            shape: SweepStadiumBorder(side: BorderSide(width: 30), style: 2),
+          ),
+        ),
+      ],
     );
   }
 }
 
 class SweepStadiumBorder extends StadiumBorder {
-  const SweepStadiumBorder({BorderSide side = BorderSide.none}) : super(side: side);
+  const SweepStadiumBorder({BorderSide side = BorderSide.none, this.style = 1}) : super(side: side);
+
+  final int style;
+
+  // final colors = [
+  //   Colors.red,
+  //   Colors.green,
+  //   Colors.blue,
+  //   Colors.amber,
+  //   Colors.pink,
+  // ];
+
+  final colors = Colors.primaries;
 
   @override
   void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {
-    final colors = [
-      Colors.red,
-      Colors.green,
-      Colors.blue,
-      Colors.amber,
-    ];
     final endColor = Color.lerp(colors.first, colors.last, 0.5)!;
     final separation = 1.0 / (colors.length + 1);
     print(List.generate(colors.length + 1, (index) => (index + .5) * separation)..insert(0, 0.0));
@@ -37,6 +54,12 @@ class SweepStadiumBorder extends StadiumBorder {
       case BorderStyle.none:
         break;
       case BorderStyle.solid:
+        if (style == 2) {
+          // For test
+          paint2(canvas, rect);
+          return;
+        }
+
         final Radius radius = Radius.circular(rect.shortestSide / 2.0);
         final paint = side.toPaint()
           ..shader = SweepGradient(
@@ -46,6 +69,22 @@ class SweepStadiumBorder extends StadiumBorder {
         canvas.drawRRect(RRect.fromRectAndRadius(rect, radius).deflate(side.width / 2.0), paint);
 
       // canvas.drawPath(getOuterPath(rect), paint);
+    }
+  }
+
+  void paint2(Canvas canvas, Rect rect, {TextDirection? textDirection}) {
+    final pathMetric = getOuterPath(rect).computeMetrics().single;
+    print(pathMetric);
+    final sep = pathMetric.length / colors.length;
+    final paint = side.toPaint();
+    for (var i = 0; i < colors.length; i++) {
+      for (var j = 0; j <= sep; j++) {
+        final start = j + sep * i;
+        canvas.drawPath(
+          pathMetric.extractPath(start, start + 1.5),
+          paint..color = Color.lerp(colors[i], colors[(i + 1) % colors.length], j / sep)!,
+        );
+      }
     }
   }
 }
