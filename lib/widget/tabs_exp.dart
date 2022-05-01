558191027c38baed26eea4c8d88bea52c662c559
Nomeleel <choonlay@outlook.com>
2022-05-01 18:24:49 +0800

    Copy TabBarView

--- lib/widget/tabs_exp.dart
+++ lib/widget/tabs_exp.dart
@@ -452,22 +452,22 @@ class _TabBarScrollController extends ScrollController {
 /// A material design widget that displays a horizontal row of tabs.
 ///
 /// Typically created as the [AppBar.bottom] part of an [AppBar] and in
-/// conjunction with a [TabBarView].
+/// conjunction with a [TabBarViewExp].
 ///
 /// {@youtube 560 315 https://www.youtube.com/watch?v=POtoEH-5l40}
 ///
 /// If a [TabController] is not provided, then a [DefaultTabController] ancestor
 /// must be provided instead. The tab controller's [TabController.length] must
 /// equal the length of the [tabs] list and the length of the
-/// [TabBarView.children] list.
+/// [TabBarViewExp.children] list.
 ///
 /// Requires one of its ancestors to be a [Material] widget.
 ///
 /// Uses values from [TabBarTheme] if it is set in the current context.
 ///
 /// {@tool dartpad --template=stateless_widget_material}
-/// This sample shows the implementation of [TabBarExp] and [TabBarView] using a [DefaultTabController].
-/// Each [Tab] corresponds to a child of the [TabBarView] in the order they are written.
+/// This sample shows the implementation of [TabBarExp] and [TabBarViewExp] using a [DefaultTabController].
+/// Each [Tab] corresponds to a child of the [TabBarViewExp] in the order they are written.
 ///
 /// ```dart
 /// Widget build(BuildContext context) {
@@ -512,7 +512,7 @@ class _TabBarScrollController extends ScrollController {
 ///
 /// {@tool dartpad --template=stateful_widget_material_ticker}
 /// [TabBarExp] can also be implemented by using a [TabController] which provides more options
-/// to control the behavior of the [TabBarExp] and [TabBarView]. This can be used instead of
+/// to control the behavior of the [TabBarExp] and [TabBarViewExp]. This can be used instead of
 /// a [DefaultTabController], demonstrated below.
 ///
 /// ```dart
@@ -566,8 +566,8 @@ class _TabBarScrollController extends ScrollController {
 ///
 /// See also:
 ///
-///  * [TabBarView], which displays page views that correspond to each tab.
-///  * [TabBarExp], which is used to display the [Tab] that corresponds to each page of the [TabBarView].
+///  * [TabBarViewExp], which displays page views that correspond to each tab.
+///  * [TabBarExp], which is used to display the [Tab] that corresponds to each page of the [TabBarViewExp].
 class TabBarExp extends StatefulWidget implements PreferredSizeWidget {
   /// Creates a material design tab bar.
   ///
@@ -615,7 +615,7 @@ class TabBarExp extends StatefulWidget implements PreferredSizeWidget {
   /// Typically a list of two or more [Tab] widgets.
   ///
   /// The length of this list must match the [controller]'s [TabController.length]
-  /// and the length of the [TabBarView.children] list.
+  /// and the length of the [TabBarViewExp.children] list.
   final List<Widget> tabs;
 
   /// This widget's selection and animation state.
@@ -1232,5 +1232,240 @@ class _TabBarExpState extends State<TabBarExp> {
     }
 
     return tabBar;
+  }
+}
+
+/// A page view that displays the widget which corresponds to the currently
+/// selected tab.
+///
+/// This widget is typically used in conjunction with a [TabBar].
+///
+/// {@youtube 560 315 https://www.youtube.com/watch?v=POtoEH-5l40}
+///
+/// If a [TabController] is not provided, then there must be a [DefaultTabController]
+/// ancestor.
+///
+/// The tab controller's [TabController.length] must equal the length of the
+/// [children] list and the length of the [TabBar.tabs] list.
+///
+/// To see a sample implementation, visit the [TabController] documentation.
+class TabBarViewExp extends StatefulWidget {
+  /// Creates a page view with one child per tab.
+  ///
+  /// The length of [children] must be the same as the [controller]'s length.
+  const TabBarViewExp({
+    Key? key,
+    required this.children,
+    this.controller,
+    this.physics,
+    this.dragStartBehavior = DragStartBehavior.start,
+  }) : super(key: key);
+
+  /// This widget's selection and animation state.
+  ///
+  /// If [TabController] is not provided, then the value of [DefaultTabController.of]
+  /// will be used.
+  final TabController? controller;
+
+  /// One widget per tab.
+  ///
+  /// Its length must match the length of the [TabBar.tabs]
+  /// list, as well as the [controller]'s [TabController.length].
+  final List<Widget> children;
+
+  /// How the page view should respond to user input.
+  ///
+  /// For example, determines how the page view continues to animate after the
+  /// user stops dragging the page view.
+  ///
+  /// The physics are modified to snap to page boundaries using
+  /// [PageScrollPhysics] prior to being used.
+  ///
+  /// Defaults to matching platform conventions.
+  final ScrollPhysics? physics;
+
+  /// {@macro flutter.widgets.scrollable.dragStartBehavior}
+  final DragStartBehavior dragStartBehavior;
+
+  @override
+  State<TabBarViewExp> createState() => _TabBarViewExpState();
+}
+
+class _TabBarViewExpState extends State<TabBarViewExp> {
+  TabController? _controller;
+  late PageController _pageController;
+  late List<Widget> _children;
+  late List<Widget> _childrenWithKey;
+  int? _currentIndex;
+  int _warpUnderwayCount = 0;
+
+  // If the TabBarView is rebuilt with a new tab controller, the caller should
+  // dispose the old one. In that case the old controller's animation will be
+  // null and should not be accessed.
+  bool get _controllerIsValid => _controller?.animation != null;
+
+  void _updateTabController() {
+    final TabController? newController = widget.controller ?? DefaultTabController.of(context);
+    assert(() {
+      if (newController == null) {
+        throw FlutterError(
+          'No TabController for ${widget.runtimeType}.\n'
+          'When creating a ${widget.runtimeType}, you must either provide an explicit '
+          'TabController using the "controller" property, or you must ensure that there '
+          'is a DefaultTabController above the ${widget.runtimeType}.\n'
+          'In this case, there was neither an explicit controller nor a default controller.',
+        );
+      }
+      return true;
+    }());
+
+    if (newController == _controller)
+      return;
+
+    if (_controllerIsValid)
+      _controller!.animation!.removeListener(_handleTabControllerAnimationTick);
+    _controller = newController;
+    if (_controller != null)
+      _controller!.animation!.addListener(_handleTabControllerAnimationTick);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _updateChildren();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _updateTabController();
+    _currentIndex = _controller?.index;
+    _pageController = PageController(initialPage: _currentIndex ?? 0);
+  }
+
+  @override
+  void didUpdateWidget(TabBarViewExp oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.controller != oldWidget.controller)
+      _updateTabController();
+    if (widget.children != oldWidget.children && _warpUnderwayCount == 0)
+      _updateChildren();
+  }
+
+  @override
+  void dispose() {
+    if (_controllerIsValid)
+      _controller!.animation!.removeListener(_handleTabControllerAnimationTick);
+    _controller = null;
+    // We don't own the _controller Animation, so it's not disposed here.
+    super.dispose();
+  }
+
+  void _updateChildren() {
+    _children = widget.children;
+    _childrenWithKey = KeyedSubtree.ensureUniqueKeysForList(widget.children);
+  }
+
+  void _handleTabControllerAnimationTick() {
+    if (_warpUnderwayCount > 0 || !_controller!.indexIsChanging)
+      return; // This widget is driving the controller's animation.
+
+    if (_controller!.index != _currentIndex) {
+      _currentIndex = _controller!.index;
+      _warpToCurrentIndex();
+    }
+  }
+
+  Future<void> _warpToCurrentIndex() async {
+    if (!mounted)
+      return Future<void>.value();
+
+    if (_pageController.page == _currentIndex!.toDouble())
+      return Future<void>.value();
+
+    final int previousIndex = _controller!.previousIndex;
+    if ((_currentIndex! - previousIndex).abs() == 1) {
+      _warpUnderwayCount += 1;
+      await _pageController.animateToPage(_currentIndex!, duration: kTabScrollDuration, curve: Curves.ease);
+      _warpUnderwayCount -= 1;
+      return Future<void>.value();
+    }
+
+    assert((_currentIndex! - previousIndex).abs() > 1);
+    final int initialPage = _currentIndex! > previousIndex
+        ? _currentIndex! - 1
+        : _currentIndex! + 1;
+    final List<Widget> originalChildren = _childrenWithKey;
+    setState(() {
+      _warpUnderwayCount += 1;
+
+      _childrenWithKey = List<Widget>.from(_childrenWithKey, growable: false);
+      final Widget temp = _childrenWithKey[initialPage];
+      _childrenWithKey[initialPage] = _childrenWithKey[previousIndex];
+      _childrenWithKey[previousIndex] = temp;
+    });
+    _pageController.jumpToPage(initialPage);
+
+    await _pageController.animateToPage(_currentIndex!, duration: kTabScrollDuration, curve: Curves.ease);
+    if (!mounted)
+      return Future<void>.value();
+    setState(() {
+      _warpUnderwayCount -= 1;
+      if (widget.children != _children) {
+        _updateChildren();
+      } else {
+        _childrenWithKey = originalChildren;
+      }
+    });
+  }
+
+  // Called when the PageView scrolls
+  bool _handleScrollNotification(ScrollNotification notification) {
+    if (_warpUnderwayCount > 0)
+      return false;
+
+    if (notification.depth != 0)
+      return false;
+
+    _warpUnderwayCount += 1;
+    if (notification is ScrollUpdateNotification && !_controller!.indexIsChanging) {
+      if ((_pageController.page! - _controller!.index).abs() > 1.0) {
+        _controller!.index = _pageController.page!.floor();
+        _currentIndex =_controller!.index;
+      }
+      _controller!.offset = (_pageController.page! - _controller!.index).clamp(-1.0, 1.0);
+    } else if (notification is ScrollEndNotification) {
+      _controller!.index = _pageController.page!.round();
+      _currentIndex = _controller!.index;
+      if (!_controller!.indexIsChanging)
+        _controller!.offset = (_pageController.page! - _controller!.index).clamp(-1.0, 1.0);
+    }
+    _warpUnderwayCount -= 1;
+
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    assert(() {
+      if (_controller!.length != widget.children.length) {
+        throw FlutterError(
+          "Controller's length property (${_controller!.length}) does not match the "
+          "number of tabs (${widget.children.length}) present in TabBar's tabs property.",
+        );
+      }
+      return true;
+    }());
+    return NotificationListener<ScrollNotification>(
+      onNotification: _handleScrollNotification,
+      child: PageView(
+        dragStartBehavior: widget.dragStartBehavior,
+        controller: _pageController,
+        physics: widget.physics == null
+          ? const PageScrollPhysics().applyTo(const ClampingScrollPhysics())
+          : const PageScrollPhysics().applyTo(widget.physics),
+        children: _childrenWithKey,
+      ),
+    );
   }
 }
