8b254fe889091fadb8ea573b41d88145a111044d
Nomeleel <choonlay@outlook.com>
2022-08-04 23:20:31 +0800

    Create multi_widget.dart

---
+++ lib/widget/multi_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class MultiTransition extends StatelessWidget {
+  const MultiTransition({Key? key, this.child, this.builder}) : super(key: key);
+  final Widget? child;
+  final WidgetBuilder? builder;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+// Multi Singlechild
+
+typedef AnimatedWidgetBuilder = Widget Function(BuildContext context, Listenable animation, Widget child);
+
+class MultiAnimatedBuilder extends StatelessWidget {
+  const MultiAnimatedBuilder({
+    Key? key,
+    required this.animation,
+    required this.builders,
+    required this.child,
+  }) : super(key: key);
+
+  final Listenable animation;
+  final List<AnimatedWidgetBuilder> builders;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) {
+        Widget widget = child!;
+        for (var builder in builders) {
+          widget = builder(context, animation, widget);
+        }
+        return widget; 
+      },
+      child: child,
+    );
+  }
+}
