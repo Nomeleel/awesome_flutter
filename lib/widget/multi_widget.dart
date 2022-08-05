629177c6393693baecbf654bfb6d074d3911c7ff
Nomeleel <choonlay@outlook.com>
2022-08-05 23:15:59 +0800

    Update multi_widget.dart

--- lib/widget/multi_widget.dart
+++ lib/widget/multi_widget.dart
@@ -36,9 +36,35 @@ class MultiAnimatedBuilder extends StatelessWidget {
         for (var builder in builders) {
           widget = builder(context, animation, widget);
         }
-        return widget; 
+        return widget;
       },
       child: child,
     );
   }
 }
+
+typedef WidgetBuilder = Widget Function(BuildContext context, Widget child);
+
+class MultiBuilder extends StatelessWidget {
+  const MultiBuilder({
+    Key? key,
+    required this.animation,
+    required this.builders,
+    required this.child,
+  }) : super(key: key);
+
+  final Listenable animation;
+  final List<WidgetBuilder> builders;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget widget = child!;
+    for (var builder in builders) {
+      widget = builder(context, widget);
+    }
+    return widget;
+  }
+}
+
+// 添加可在中间插入的builder
