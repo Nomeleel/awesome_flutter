57c2cb7b7ef5d855207071c0b529b6079cbe763c
Choonlay Lee <choonlay@outlook.com>
2022-12-20 06:33:34 +0000

    Refine inner layout.

--- lib/layout/layout_notifier.dart
+++ lib/layout/layout_notifier.dart
@@ -3,35 +3,38 @@ import 'package:flutter/material.dart';
 import 'adaptive.dart';
 
 class Layout extends StatelessWidget {
-  const Layout({Key? key, required this.child}) : super(key: key);
+  const Layout({super.key, required this.child});
 
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    bool useConstraints = true;
-    final child = LayoutBuilder(
+    final useConstraints = ValueNotifier(true);
+
+    Widget wrapSwitchBuilder(Widget body) {
+      return Stack(
+        alignment: Alignment.topRight,
+        children: [
+          body,
+          GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () => useConstraints.value = !useConstraints.value,
+            child: Icon(
+              useConstraints.value ? Icons.fullscreen : Icons.fullscreen_exit,
+            ),
+          ),
+        ],
+      );
+    }
+
+    final body = LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
         final preferDesktop = isDisplayDesktop(constraints.biggest);
-        return StatefulBuilder(
-          builder: (context, setState) {
-            Widget wrapSwitchBuilder(Widget body) {
-              return Stack(
-                alignment: Alignment.topRight,
-                children: [
-                  body,
-                  GestureDetector(
-                    behavior: HitTestBehavior.opaque,
-                    onTap: () => setState(() => useConstraints = !useConstraints),
-                    child: Icon(
-                      useConstraints ? Icons.fullscreen : Icons.fullscreen_exit,
-                    ),
-                  ),
-                ],
-              );
-            }
 
-            if (useConstraints && preferDesktop) {
+        return ValueListenableBuilder<bool>(
+          valueListenable: useConstraints,
+          builder: (context, useConstraints, body) {
+            if (preferDesktop && useConstraints) {
               return Container(
                 padding: EdgeInsets.all(constraints.maxHeight / 7.0),
                 alignment: Alignment.center,
@@ -45,13 +48,13 @@ class Layout extends StatelessWidget {
                   AspectRatio(
                     aspectRatio: 727.0 / 1468.0,
                     child: Stack(
-                      textDirection: TextDirection.ltr,
+                      alignment: Alignment.topRight,
                       children: <Widget>[
                         Padding(
                           padding: EdgeInsets.all(constraints.maxHeight / 90.0),
                           child: ClipRRect(
                             borderRadius: BorderRadius.circular(constraints.maxHeight / 25.0),
-                            child: this.child,
+                            child: body,
                           ),
                         ),
                         // ignore: prefer_asset_const
@@ -63,17 +66,18 @@ class Layout extends StatelessWidget {
               );
             }
 
-            if (preferDesktop) return wrapSwitchBuilder(this.child);
+            if (preferDesktop) return wrapSwitchBuilder(body!);
 
-            return this.child;
+            return body!;
           },
+          child: child,
         );
       },
     );
 
     return Directionality(
       textDirection: Directionality.maybeOf(context) ?? TextDirection.ltr,
-      child: child,
+      child: body,
     );
   }
 }
